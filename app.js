0878bb44af2858e49d2441d77139af0126b9f83c
fora-heims <foraheims@gmail.com>
2021-10-08 13:23:08 -0700

    Generate random number feature is functional. All integration tests passing.

--- app.js
+++ app.js
@@ -5,7 +5,11 @@ const paragraph = document.getElementById('random-num');
 // initialize global state
 
 // set event listeners 
-// get user input
+button.addEventListener('click', () => {
 // use user input to update state 
+    let x = Math.floor(Math.random() * 10000);
 // update DOM to reflect the new state
+    paragraph.textContent = x;
+});
+
 
